067a8c06eba6e2168630d58dbc9d3f0041a9942b
thisisgoldman <emgoldma@gmail.com>
2021-05-11 21:52:18 -0700

    Add all dependencies

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,15 +1,20 @@
-import Head from "next/head";
-import "../styles/tailwind.css";
+import Head from 'next/head'
+import { createClient, Provider } from 'urql'
+import '../styles/tailwind.css'
+
+const client = createClient({
+  url: 'http://localhost:4000/graphql',
+})
 
 const MyApp = ({ Component, pageProps }) => {
   return (
-    <>
+    <Provider value={client}>
       <Head>
         <title>Startup Acquisitions</title>
       </Head>
       <Component {...pageProps} />
-    </>
-  );
-};
+    </Provider>
+  )
+}
 
-export default MyApp;
+export default MyApp
